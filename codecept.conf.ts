6fbb1084f6d4908009c90d5778a1c59bba2044ba
hnu <hnu@sucafina.com>
2025-07-24 15:46:14 +0700

    chore: initialize CodeceptJS project with TypeScript configuration and basic test structure

---
+++ codecept.conf.ts
@@ -0,0 +1,24 @@
+import { setHeadlessWhen, setCommonPlugins } from '@codeceptjs/configure';
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+// enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
+setCommonPlugins();
+
+export const config: CodeceptJS.MainConfig = {
+  tests: './*_test.ts',
+  output: './output',
+  helpers: {
+    Playwright: {
+      browser: 'chromium',
+      url: 'https://stockkqa.sucafina.corp/',
+      show: true
+    }
+  },
+  include: {
+    I: './steps_file'
+  },
+  name: 'codeceptjs',
+  fullPromiseBased: true,
+}
