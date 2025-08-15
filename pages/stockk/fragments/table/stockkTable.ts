89b881ebd1e3280e368085d3bf5c9a9f47da77f9
hnu <hnu@sucafina.com>
2025-08-15 14:19:26 +0700

    ongoing table component

---
+++ pages/stockk/fragments/table/stockkTable.ts
@@ -0,0 +1,13 @@
+const { I } = inject();
+
+class stockkTable {
+  
+}
+
+
+// Export both the class and an instance
+export default stockkTable;
+
+// For CodeceptJS compatibility
+module.exports = new stockkTable();
+module.exports.default = stockkTable;
